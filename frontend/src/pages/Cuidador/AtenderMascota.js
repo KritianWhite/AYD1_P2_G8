3ae3f72359667efe1c010c90659f67b55dde55e3
Luis <3154826051301@ingenieria.usac.edu.gt>
2023-12-20 13:56:59 -0600

    Fixes cuidador

--- frontend/src/pages/Cuidador/AtenderMascota.js
+++ frontend/src/pages/Cuidador/AtenderMascota.js
@@ -1,83 +1,75 @@
 import React, { useState, useEffect } from "react";
-
 import NavbarCuidador from "./NavbarCuidador.js";
-
 import "./Styles/MisLibros.css";
 
 export default function AtenderMascota() {
   const [mascotas, setMascotas] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState({});
 
   const formatearFecha = (fechaOriginal) => {
     const fecha = new Date(fechaOriginal);
     return fecha.toISOString().substring(0, 10);
   };
 
+  const handleOptionChange = (index, e) => {
+    const newOptions = { ...selectedOptions, [index]: e.target.value };
+    setSelectedOptions(newOptions);
+  };
+
+  const handleSaveOption = (index) => {
+    // Aquí puedes usar la opción seleccionada (selectedOptions[index]) como desees
+    console.log(`Opción seleccionada para mascota ${index}:`, selectedOptions[index]);
+  };
+
   useEffect(() => {
-    fetch(
-      `http://localhost:4000/usuario/cuidador_mascota/mariacuidador@email.com`,
-      {
+    if (localStorage.getItem("correo") == null) {
+      console.log("No hay usuario");
+      window.location.href = "http://localhost:3000/";
+    } else {
+      const user = localStorage.getItem("correo").replace(/"/g, "");
+      fetch(`http://localhost:4000/usuario/cuidador_mascota/${user}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-      }
-    )
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log("Error:", err);
       })
-      .then((response) => {
-        console.log(response);
-        //setMascotas(response);
-/*         if (
-          response.message !=
-          "No se encontraron libros rentados/comprados por el usuario"
-        ) {
+        .then((res) => res.json())
+        .catch((err) => {
+          console.log("Error:", err);
+        })
+        .then((response) => {
+         // console.log("Respuesta de mascotas:", response);
           setMascotas(response);
-        } else {
-          setMascotas([
-            {
-              anio_publicacion: 2023,
-              autor: "Nombre del autor",
-              editorial: "Editorial",
-              estado: "rentado",
-              fecha_devolucion_renta: "2021-05-03T06:00:00.000Z",
-              id_libro: 5,
-              portada: null,
-              precio_compra: 0,
-              precio_venta: 0,
-              sinopsis: "Sinopsis",
-              titulo: "Titulo",
-            },
-          ]);
-        } */
-      });
-
-/*     if (localStorage.getItem("usuario") == null) {
-      console.log("No hay usuario");
-      //window.location.href = "http://localhost:3000/";
-    } else {
-      //const user =  JSON.parse(localStorage.getItem("usuario")).replace(/"/g, "");
-      //`http://localhost:4000/usuario/biblioteca/${user}`
-      
-    } */
+        });
+    }
   }, []);
 
   return (
     <>
       <NavbarCuidador />
-      <div class="container">
-        <a>Atender mascota</a>
-        {mascotas.map((mascota, index) => {
-          return (
-            <div key={index} class="card-mislibros">
-              <img src="https://img2.wallspic.com/previews/2/9/0/4/6/164092/164092-samsung_galaxy-samsung-smartphone-water-liquid-x750.jpg" />
-              <h3>{mascota.nombre}</h3>
-              <p>Especie: {mascota.especie}</p>
+      <div className="container">
+        {mascotas.map((mascota, index) => (
+          <div key={index} className="card-mislibros">
+            <img src="https://img2.wallspic.com/previews/2/9/0/4/6/164092/164092-samsung_galaxy-samsung-smartphone-water-liquid-x750.jpg" alt="Mascota" />
+            <h3>{mascota.nombre}</h3>
+            <p>Especie: {mascota.especie}</p>
+            <p>Raza: {mascota.raza}</p>
+            <p>Edad: {mascota.edad}</p>
 
-            </div>
-          );
-        })}
+            {/* Agregar el cuadro de opciones (select) */}
+            <select value={selectedOptions[index] || ""} onChange={(e) => handleOptionChange(index, e)}>
+              <option value="">Seleccionar actividad</option>
+              <option value="comiendo">Comiendo</option>
+              <option value="paseando">Paseando</option>
+              <option value="bañando">Bañando</option>
+              <option value="tomando-siesta">Tomando la siesta</option>
+              <option value="jugando">Jugando</option>
+            </select>
+
+            {/* Botón para guardar la opción seleccionada */}
+            <button onClick={() => handleSaveOption(index)}>Guardar</button>
+          </div>
+        ))}
       </div>
     </>
   );
